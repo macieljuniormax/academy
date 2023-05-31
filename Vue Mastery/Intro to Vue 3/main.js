7b6f27d6a22a18b839926ccd21431d0206456688
macieljuniormax <macielfcjunior@icloud.com>
2023-05-30 21:40:18 -0300

    feat: :sparkles: using the binding attribute

--- Vue Mastery/Intro to Vue 3/main.js
+++ Vue Mastery/Intro to Vue 3/main.js
@@ -2,7 +2,9 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            description: 'A pair of warm, fuzzy socks'
+            image: './assets/images/socks_blue.jpg',
+            description: 'A pair of warm, fuzzy socks',
+            url: 'https:/github.com/macieljuniormax',
         }
     }
 })
