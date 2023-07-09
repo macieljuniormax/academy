4cdb2adc713f44f7e693a9814db1a5decab2fbba
macieljuniormax <macielfcjunior@icloud.com>
2023-07-09 20:35:45 -0300

    feat: create computed properties

--- Vue Mastery/Intro to Vue 3/main.js
+++ Vue Mastery/Intro to Vue 3/main.js
@@ -3,17 +3,17 @@ const app = Vue.createApp({
         return {
             cart: 0,
             product: 'Socks',
-            image: './assets/images/socks_blue.jpg',
+            selectedVariant: 0,
             description: 'A pair of warm, fuzzy socks',
             url: 'https:/github.com/macieljuniormax',
-            inStock: false,
             inventory: 10,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' }
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 0 },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 10 }
             ],
             sizes: ['S', 'M', 'L', 'XL', 'XXL'],
+            brand: 'Vue Mastery',
         }
     },
 
@@ -21,8 +21,20 @@ const app = Vue.createApp({
         addToCart() {
             this.cart += 1
         },
-        updateImage(variantImage) {
-            this.image = variantImage
+        updateVariant(index) {
+            this.selectedVariant = index
+        }
+    },
+
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].image
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity
         }
     }
 })
