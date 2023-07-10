c0a3731d7dee1a0a4c95fb4b5c69cfb8bf1e491c
macieljuniormax <macielfcjunior@icloud.com>
2023-07-10 15:01:44 -0300

    feat: create components

--- Vue Mastery/Intro to Vue 3/main.js
+++ Vue Mastery/Intro to Vue 3/main.js
@@ -1,40 +1,10 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            cart: 0,
-            product: 'Socks',
-            selectedVariant: 0,
-            description: 'A pair of warm, fuzzy socks',
-            url: 'https:/github.com/macieljuniormax',
-            inventory: 10,
-            details: ['50% cotton', '30% wool', '20% polyester'],
-            variants: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 0 },
-                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 10 }
-            ],
-            sizes: ['S', 'M', 'L', 'XL', 'XXL'],
-            brand: 'Vue Mastery',
-        }
-    },
+  data() {
+    return {
+      cart: 0,
+      premium: true,
+    };
+  },
 
-    methods: {
-        addToCart() {
-            this.cart += 1
-        },
-        updateVariant(index) {
-            this.selectedVariant = index
-        }
-    },
-
-    computed: {
-        title() {
-            return this.brand + ' ' + this.product
-        },
-        image() {
-            return this.variants[this.selectedVariant].image
-        },
-        inStock() {
-            return this.variants[this.selectedVariant].quantity
-        }
-    }
-})
+  methods: {},
+});
